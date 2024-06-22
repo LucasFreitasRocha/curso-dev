c9c835d7903af0e6428b9bd5e9f094497ec48a23
Lucas Freitas da Rocha <rochadefreitaslucas@gmail.com>
2024-06-22 12:15:13 -0300

    feat: creating project page under building

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 function Home() {
     return (
         <>
-        <h1>Te amo b! Vem aprender programação comigo!</h1>
+          <h1>Projeto em construção</h1>
+          <h2>Esse projeto é referente ao curso do filipe deschamps, do <a  href="https://curso.dev/"  target="_blank">curso.dev</a></h2>
         </>
     )
 }
